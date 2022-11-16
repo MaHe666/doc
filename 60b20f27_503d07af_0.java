8165dd3a2efa0b214eba09148dbc9435ed49920a
MaHe666 <m13234666930@163.com>
2022-11-16 20:49:03 +0800

    2022.11.16

---
+++ 60b20f27_503d07af_0.java
@@ -0,0 +1,13 @@
+package com.panda.common.base;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * 基础服务实现类
+ *
+ * @className: BaseServiceImpl
+ * @author: GuoShunFa
+ * @date: 2022/11/10
+ **/
+public class BaseServiceImpl<M, T> extends ServiceImpl implements BaseService<T> {
+}

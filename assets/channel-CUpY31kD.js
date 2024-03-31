0c95eb0e8af16eaa5abaffb9a387da4809ce0ada
MaHe666 <74131961+MaHe666@users.noreply.github.com>
2024-03-31 06:29:06 +0000

    Update main to output generated at 92483f1

--- assets/channel-CUpY31kD.js
+++
@@ -1,1 +0,0 @@
-import{am as n,an as o}from"./mermaid.core-DSPeO2DD.js";const t=(a,r)=>n.lang.round(o.parse(a)[r]);export{t as c};
---
+++ assets/报文.html-Dq7QQk8z.js
@@ -0,0 +1,34 @@
+import{_ as t}from"./plugin-vue_export-helper-DlAUqK2U.js";import{r as a,o as d,c as s,d as e,e as n,a as c,f as l}from"./app-CjoW4Pl-.js";const o={},r=e("h2",{id:"通用",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#通用"},[e("span",null,"通用")])],-1),v=e("p",null,"百度 的 通用报文：",-1),_=e("code",null,"Request URL",-1),u={href:"https://www.baidu.com/",target:"_blank",rel:"noopener noreferrer"},m=l(`<p><code>Request Method</code>: GET</p><p><code>Status Code</code>: 200 OK</p><p><code>Remote Address</code>: 110.242.68.3:443</p><p><code>Referrer Policy</code>: strict-origin-when-cross-origin</p><h2 id="请求" tabindex="-1"><a class="header-anchor" href="#请求"><span>请求</span></a></h2><p><code>HTTP请求</code></p><p>客户端 ---请求（Request）---&gt; 服务器</p><p>百度 请求报文：</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9		//支持的格式
+Accept-Encoding: gzip, deflate, br		//编码
+Accept-Language: zh-CN,zh;q=0.9		//语言
+Cache-Control: max-age=0			//缓存控制
+Connection: keep-alive			//连接，保持活跃
+Cookie: BIDUPSID=2301D0A04DE8502099E8EDEC3A7D0732; PSTM=1628494350; BAIDUID=2301D0A04DE85020C570129D2270C5F5:FG=1; BD_UPN=12314753; Hm_lvt_aec699bb6442ba076c8981c6dc490771=1631232376,1631411460; BAIDUID_BFESS=8EFCA68F62C3E7C523ABB272127319EE:FG=1; __yjs_duid=1_59ee9767df782c5b5c2c7ffaa4d1c77f1631412398153; COOKIE_SESSION=136894_0_9_9_27_38_1_0_9_8_1733_10_136893_0_39_0_1632357568_0_1632357529%7C9%230_0_1632357529%7C1; BD_HOME=1; H_PS_PSSID=34650_34068_31253_34553_34741_34525_34584_34707_34518_26350_34418_34691_34679; BA_HECTOR=al0020248l8lah25q51glcv4k0q
+Host: www.baidu.com
+sec-ch-ua: &quot;Chromium&quot;;v=&quot;94&quot;, &quot;Google Chrome&quot;;v=&quot;94&quot;, &quot;;Not A Brand&quot;;v=&quot;99&quot;
+sec-ch-ua-mobile: ?0
+sec-ch-ua-platform: &quot;Windows&quot;
+Sec-Fetch-Dest: document
+Sec-Fetch-Mode: navigate
+Sec-Fetch-Site: none
+Sec-Fetch-User: ?1
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36	//浏览器的信息
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="响应" tabindex="-1"><a class="header-anchor" href="#响应"><span>响应</span></a></h2><p><code>HTTP响应</code></p><p>服务器 ---响应---&gt; 客户端</p><p>百度 响应报文：</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>Bdpagetype: 1
+Bdqid: 0xd0fce91d0005a766			//这两个不用管，固定的ID，服务器返回的，为了监控你的电脑
+Cache-Control: private			//缓存控制
+Connection: keep-alive			//连接，保持活跃		
+Content-Encoding: gzip			//编码
+Content-Type: text/html;charset=utf-8	//类型
+Date: Fri, 01 Oct 2021 03:12:24 GMT		//时间
+Expires: Fri, 01 Oct 2021 03:11:46 GMT	//时间
+Server: BWS/1.1				//浏览器服务信息
+Set-Cookie: BDSVRTM=0; path=/
+Set-Cookie: BD_HOME=1; path=/
+Set-Cookie: H_PS_PSSID=34650_34068_31253_34553_34741_34525_34584_34707_34518_26350_34418_34691_34679; path=/; domain=.baidu.com
+Strict-Transport-Security: max-age=172800		//与安全有关
+Traceid: 1633057944359734708215059167564783789926
+Transfer-Encoding: chunked
+X-Frame-Options: sameorigin
+X-Ua-Compatible: IE=Edge,chrome=1	//浏览器的信息
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,14);function p(b,h){const i=a("ExternalLinkIcon");return d(),s("div",null,[r,v,e("p",null,[_,n(": "),e("a",u,[n("https://www.baidu.com/"),c(i)])]),m])}const C=t(o,[["render",p],["__file","报文.html.vue"]]);export{C as default};
